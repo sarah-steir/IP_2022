44d04d97fba196ac76a008dbbdf2ad186af81dbf
natyluh <93599526+natyluh@users.noreply.github.com>
2022-01-18 18:35:47 -0500

    Update HelloApplication.java

--- src/main/java/com/example/ip_2022/HelloApplication.java
+++ src/main/java/com/example/ip_2022/HelloApplication.java
@@ -12,10 +12,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("!");
+        //Sarahhhhhhhh
         //Natalia
         stage.setScene(scene);
         stage.show();
+        
+          // Tayba
     }
 
     public static void main(String[] args) {
