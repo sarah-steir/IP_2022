4146c2c6c9b0d9b29393a3692203ff9bf1e30f95
sarah-steir <sarah@mysteiners.com>
2022-01-18 18:13:49 -0500

    comment to check that repo works

--- src/main/java/com/example/ip_2022/HelloApplication.java
+++ src/main/java/com/example/ip_2022/HelloApplication.java
@@ -13,6 +13,10 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        //Sarahhhhhhhh
+
+
+
         stage.setScene(scene);
         stage.show();
     }
