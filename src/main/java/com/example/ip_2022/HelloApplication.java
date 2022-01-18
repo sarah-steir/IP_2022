79ea91d18f3b586cc7deab0e86240c1fb9517d16
TJusab <tayba.jusab25@gmail.com>
2022-01-18 18:20:11 -0500

    Tayba's test comment

--- src/main/java/com/example/ip_2022/HelloApplication.java
+++ src/main/java/com/example/ip_2022/HelloApplication.java
@@ -19,6 +19,8 @@ public class HelloApplication extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        // Tayba
     }
 
     public static void main(String[] args) {
