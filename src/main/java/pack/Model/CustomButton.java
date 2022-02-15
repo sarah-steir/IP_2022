8c2dfb91de3e6046a52700a7b6024a466f34303f
constance <knztnce@gmail.com>
2022-02-15 12:34:35 -0500

    changed the font and Tayba is here :)

--- src/main/java/pack/Model/CustomButton.java
+++ src/main/java/pack/Model/CustomButton.java
@@ -7,13 +7,11 @@ import static pack.Model.mainModel.p;
 
 public class CustomButton extends Button {
 
-    Font font = new Font(p + "Font.ttf", 15);
-
     public CustomButton(String message) {
         super(message);
         this.setStyle("-fx-background-color: #333335;" +
                 "-fx-text-fill: E7EBEE;");
-        this.setFont(font);
+        this.setFont(mainModel.font);
         this.setMinSize(115, 30);
     }
 
--- src/main/java/pack/Model/mainModel.java
+++ src/main/java/pack/Model/mainModel.java
@@ -1,10 +1,14 @@
 package pack.Model;
 
+import javafx.scene.text.Font;
+
 import java.io.File;
 
 public class mainModel {
 
     static File f = new File("");
     public static String p = "file:" + f.getAbsolutePath() + "/";
+
+    public static Font font = Font.loadFont(p + "Font.otf", 15);
 
 }
