6ab2650eb875536e86a6aba6a5415d05d71602af
TJusab <tayba.jusab25@gmail.com>
2022-02-21 13:44:55 -0500

    view and model changes

---
+++ src/main/java/pack/View/iView.java
@@ -0,0 +1,89 @@
+package pack.View;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import pack.Model.CustomButton;
+import pack.Model.mainModel;
+
+import static pack.Model.mainModel.p;
+
+public interface iView {
+
+    static HBox setButtons() {
+        Button btnStart = new CustomButton("Start\nthe\n MAGIK");
+        Button btnReset= new CustomButton("Reset\nthe\nMAGIK");
+        HBox hbButtons= new HBox();
+        hbButtons.setSpacing(10);
+        hbButtons.getChildren().addAll(btnStart,btnReset);
+        btnStart.setOnAction(e -> System.out.println("Patate"));
+        btnReset.setOnAction(e -> System.out.println("Reset patate"));
+        return hbButtons;
+    }
+
+    static ImageView setLogo() {
+        ImageView iv = new ImageView(new Image(p + "Logo.png"));
+        iv.setFitWidth(225);
+        iv.setFitHeight(105);
+        return iv;
+    }
+
+    static VBox setLeft() {
+        VBox vbLeft = new VBox();
+        vbLeft.setSpacing(10);
+        vbLeft.setPrefSize(500, 695);
+        vbLeft.setLayoutX(10);
+        vbLeft.setLayoutY(14);
+
+        // User Input Box
+        VBox vbUi = new VBox();
+        vbUi.setPrefSize(500, 160);
+        vbUi.setStyle("-fx-background-color: #333335"); // Grey
+
+        // Graph Box
+        Pane graph = new Pane();
+        graph.setPrefSize(500, 525);
+        graph.setStyle("-fx-background-color: #333335");    // Grey
+
+        vbLeft.getChildren().addAll(vbUi, graph);
+        return vbLeft;
+    }
+
+    static VBox setRight(String title) {
+        VBox vbRight = new VBox();
+        vbRight.setPrefSize(500, 695);
+        vbRight.setLayoutX(520);
+        vbRight.setLayoutY(14);
+        vbRight.setSpacing(10);
+
+        // Program Output Box
+        VBox vbPo = new VBox();
+        vbPo.setPrefSize(500, 595);
+        vbPo.setSpacing(15);
+        vbPo.setAlignment(Pos.TOP_CENTER);
+        vbPo.setStyle("-fx-background-color: #333335");
+        vbPo.getChildren().add(mainModel.setTitle(title));
+
+        // HBox to hold buttons and logo
+        HBox hbBottom = new HBox();
+        hbBottom.setMaxSize(500, 105);
+        hbBottom.setSpacing(55);
+        hbBottom.getChildren().addAll(setButtons(), setLogo());
+
+        vbRight.getChildren().addAll(vbPo, hbBottom);
+        return vbRight;
+    }
+
+    static Pane setView() {
+        Pane pane = new Pane();
+        pane.setPrefSize(1050, 750);
+        pane.setStyle("-fx-background-color: #6F6F77;");
+        pane.getChildren().add(setLeft());
+        pane.getChildren().add(setRight("system of linear equations"));
+        return pane;
+    }
+}
