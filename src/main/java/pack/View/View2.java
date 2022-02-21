2be055a207dfb423b81108cbd3b5502c36c0f883
nasjuice <nasjuice7@gmail.com>
2022-02-21 16:12:05 -0500

    more interface stuff

--- src/main/java/pack/View/View2.java
+++ src/main/java/pack/View/View2.java
@@ -11,19 +11,7 @@ import static pack.Model.mainModel.p;
 public class View2 extends Pane{
 
     public View2() {
-        this.setPrefSize(1050, 750);
-        this.setStyle("-fx-background-color: #6F6F77;");
-        this.setLogo();
-    }
-
-    public Node setLogo() {
-        ImageView iv = new ImageView(new Image(p + "Logo.png"));
-        this.getChildren().add(iv);
-        iv.setFitWidth(225);
-        iv.setFitHeight(100);
-        iv.setLayoutX(805);
-        iv.setLayoutY(610);
-        return iv;
+        this.getChildren().add(iView.setView("Eigen UI"));
     }
 
 }
