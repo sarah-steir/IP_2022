3f260b05af16a62ad4d83e69bb5f5b3b2dfe98d2
constance <knztnce@gmail.com>
2022-03-10 21:20:58 -0500

    Updated a comment in the main because I'm a perfectionist and for my own sanity Fait le ménage des imports dans le View1 le View2 et le View3 for my own sanity as well Apparently I also changed something in the mainView but I can't find what

--- src/main/java/pack/View/View1.java
+++ src/main/java/pack/View/View1.java
@@ -1,25 +1,7 @@
 package pack.View;
 
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
-
-import javafx.scene.text.Text;
-import pack.Model.CustomButton;
-import pack.Model.mainModel;
-
-import java.util.ArrayList;
-
-import static pack.Model.mainModel.p;
 
 public class View1 extends Pane implements iView {
     static RadioButton twoo = new RadioButton("2by2");
--- src/main/java/pack/View/View3.java
+++ src/main/java/pack/View/View3.java
@@ -1,16 +1,6 @@
 package pack.View;
 
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
-
-import pack.Model.CustomButton;
-import static pack.Model.mainModel.p;
-
 
 public class View3 extends Pane implements iView{
 
