9308ad362dcdad72d03f3837b0f836747b15ce04
Constance <79022298+knstnce@users.noreply.github.com>
2022-04-11 09:37:38 -0400

    removed an import

--- src/main/java/pack/main.java
+++ src/main/java/pack/main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import pack.Model.ModelForJSON;
 import pack.View.MainView;
 
 public class main extends Application {
