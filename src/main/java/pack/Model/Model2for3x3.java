f793388f78469703434265c42722b382925cbfb5
Constance <79022298+knstnce@users.noreply.github.com>
2022-04-21 12:47:14 -0400

    format shit

--- src/main/java/pack/Model/Model2for3x3.java
+++ src/main/java/pack/Model/Model2for3x3.java
@@ -134,10 +134,9 @@ public class Model2for3x3 {
 
         }
 
-
-        x1 = Double.parseDouble(formatting.format(x1)); // FORMAT TO 0.0000
-        x2 = Double.parseDouble(formatting.format(x2));
-        x3 = Double.parseDouble(formatting.format(x3));
+        x1 = Double.parseDouble(String.valueOf(x1)); // FORMAT TO 0.0000
+        x2 = Double.parseDouble(String.valueOf(x2));
+        x3 = Double.parseDouble(String.valueOf(x3));
 
         System.out.println("x1: " + x1 + "x2: " + x2 + "x3: " + x3);
     }
@@ -168,107 +167,107 @@ public class Model2for3x3 {
 
     // reduces the matrix
     private double[] reduceMatrix3x3(double a1, double a2, double a3, double b1, double b2, double b3, double c1, double c2, double c3) {
-        a1 = Double.parseDouble(formatting.format(a1)); // format everything
-        a2 = Double.parseDouble(formatting.format(a2));
-        a3 = Double.parseDouble(formatting.format(a3));
-        b1 = Double.parseDouble(formatting.format(b1));
-        b2 = Double.parseDouble(formatting.format(b2));
-        b3 = Double.parseDouble(formatting.format(b3));
-        c1 = Double.parseDouble(formatting.format(c1));
-        c2 = Double.parseDouble(formatting.format(c2));
-        c3 = Double.parseDouble(formatting.format(c3));
+        a1 = Double.parseDouble(String.valueOf(a1)); // format everything
+        a2 = Double.parseDouble(String.valueOf(a2));
+        a3 = Double.parseDouble(String.valueOf(a3));
+        b1 = Double.parseDouble(String.valueOf(b1));
+        b2 = Double.parseDouble(String.valueOf(b2));
+        b3 = Double.parseDouble(String.valueOf(b3));
+        c1 = Double.parseDouble(String.valueOf(c1));
+        c2 = Double.parseDouble(String.valueOf(c2));
+        c3 = Double.parseDouble(String.valueOf(c3));
         if (a1 == 0) { // if a equal to zero (switch rows) unless all is zero (a,b,c)
             if (b1 == 0) { // if second row also has zero as the first number
                 if (c1 != 0) { // then switch a with c if c not equal zero
-                    e1 = Double.parseDouble(formatting.format(a1)); // E holds the number
-                    e2 = Double.parseDouble(formatting.format(a2));
-                    e3 = Double.parseDouble(formatting.format(a3));
-                    a1 = Double.parseDouble(formatting.format(c1));
-                    a2 = Double.parseDouble(formatting.format(c2));
-                    a3 = Double.parseDouble(formatting.format(c3));
-                    c1 = Double.parseDouble(formatting.format(e1));
-                    c2 = Double.parseDouble(formatting.format(e2));
-                    c3 = Double.parseDouble(formatting.format(e3));
+                    e1 = Double.parseDouble(String.valueOf(a1)); // E holds the number
+                    e2 = Double.parseDouble(String.valueOf(a2));
+                    e3 = Double.parseDouble(String.valueOf(a3));
+                    a1 = Double.parseDouble(String.valueOf(c1));
+                    a2 = Double.parseDouble(String.valueOf(c2));
+                    a3 = Double.parseDouble(String.valueOf(c3));
+                    c1 = Double.parseDouble(String.valueOf(e1));
+                    c2 = Double.parseDouble(String.valueOf(e2));
+                    c3 = Double.parseDouble(String.valueOf(e3));
                 }
             }
             if (b1 != 0) { // switch a with b if b not equal zero
 
-                e1 = Double.parseDouble(formatting.format(a1));
-                e2 = Double.parseDouble(formatting.format(a2));
-                e3 = Double.parseDouble(formatting.format(a3));
-                a1 = Double.parseDouble(formatting.format(b1));
-                a2 = Double.parseDouble(formatting.format(b2));
-                a3 = Double.parseDouble(formatting.format(b3));
-                b1 = Double.parseDouble(formatting.format(e1));
-                b2 = Double.parseDouble(formatting.format(e2));
-                b3 = Double.parseDouble(formatting.format(e3));
+                e1 = Double.parseDouble(String.valueOf(a1));
+                e2 = Double.parseDouble(String.valueOf(a2));
+                e3 = Double.parseDouble(String.valueOf(a3));
+                a1 = Double.parseDouble(String.valueOf(b1));
+                a2 = Double.parseDouble(String.valueOf(b2));
+                a3 = Double.parseDouble(String.valueOf(b3));
+                b1 = Double.parseDouble(String.valueOf(e1));
+                b2 = Double.parseDouble(String.valueOf(e2));
+                b3 = Double.parseDouble(String.valueOf(e3));
             }
         }
         if (a1 != 1 && a1 != 0) { //if a not equal to one or zero
 
-            a2 = Double.parseDouble(formatting.format(a2 / a1)); //MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
-            a3 = Double.parseDouble(formatting.format(a3 / a1));
-            a1 = Double.parseDouble(formatting.format(a1 / a1));
+            a2 = Double.parseDouble(String.valueOf(a2 / a1)); //MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
+            a3 = Double.parseDouble(String.valueOf(a3 / a1));
+            a1 = Double.parseDouble(String.valueOf(a1 / a1));
         }
         if (b1 != 0) { // reduce the second row to 0 in the first #
 
-            b2 = Double.parseDouble(formatting.format(b2 - (a2 * b1)));//MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
-            b3 = Double.parseDouble(formatting.format(b3 - (a3 * b1)));
-            b1 = Double.parseDouble(formatting.format(b1 - (a1 * b1)));
+            b2 = Double.parseDouble(String.valueOf(b2 - (a2 * b1)));//MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
+            b3 = Double.parseDouble(String.valueOf(b3 - (a3 * b1)));
+            b1 = Double.parseDouble(String.valueOf(b1 - (a1 * b1)));
         }
         if (c1 != 0) { // reduce the third row to 0 in the first #
 
-            c2 = Double.parseDouble(formatting.format(c2 - (a2 * c1)));//MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
-            c3 = Double.parseDouble(formatting.format(c3 - (a3 * c1)));
-            c1 = Double.parseDouble(formatting.format(c1 - (a1 * c1)));
+            c2 = Double.parseDouble(String.valueOf(c2 - (a2 * c1)));//MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
+            c3 = Double.parseDouble(String.valueOf(c3 - (a3 * c1)));
+            c1 = Double.parseDouble(String.valueOf(c1 - (a1 * c1)));
         }
         //COLUMN ONE DONE
         if (b2 == 0) { // if a equal to zero (switch rows) unless all is zero (a,b,c)
             if (c2 != 0) {
-                e2 = Double.parseDouble(formatting.format(b2)); // replace b with c if c not zero
-                e3 = Double.parseDouble(formatting.format(b3));
-                b2 = Double.parseDouble(formatting.format(c2));
-                b3 = Double.parseDouble(formatting.format(c3));
-                c2 = Double.parseDouble(formatting.format(e2));
-                c3 = Double.parseDouble(formatting.format(e3));
+                e2 = Double.parseDouble(String.valueOf(b2)); // replace b with c if c not zero
+                e3 = Double.parseDouble(String.valueOf(b3));
+                b2 = Double.parseDouble(String.valueOf(c2));
+                b3 = Double.parseDouble(String.valueOf(c3));
+                c2 = Double.parseDouble(String.valueOf(e2));
+                c3 = Double.parseDouble(String.valueOf(e3));
             }
             if (b2 == 0 && c2 == 0 && a1 == 0 && a2 != 0) { // if A has 0 in the first position and not zero in second position a switch with b
-                e2 = Double.parseDouble(formatting.format(b2));
-                e3 = Double.parseDouble(formatting.format(b3));
-                b2 = Double.parseDouble(formatting.format(a2));
-                b3 = Double.parseDouble(formatting.format(a3));
-                a2 = Double.parseDouble(formatting.format(e2));
-                a3 = Double.parseDouble(formatting.format(e3));
+                e2 = Double.parseDouble(String.valueOf(b2));
+                e3 = Double.parseDouble(String.valueOf(b3));
+                b2 = Double.parseDouble(String.valueOf(a2));
+                b3 = Double.parseDouble(String.valueOf(a3));
+                a2 = Double.parseDouble(String.valueOf(e2));
+                a3 = Double.parseDouble(String.valueOf(e3));
             }
         }
         if (b2 != 1 && b2 != 0) { //if a not equal to one or zero
-            b3 = Double.parseDouble(formatting.format(b3 / b2));//MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
-            b2 = Double.parseDouble(formatting.format(b2 / b2));
+            b3 = Double.parseDouble(String.valueOf(b3 / b2));//MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
+            b2 = Double.parseDouble(String.valueOf(b2 / b2));
         }
 
         if (a2 != 0) { // reduce the second row to 0 in the first #
-            a3 = Double.parseDouble(formatting.format(a3 - (a2 * b3))); //MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
-            a2 = Double.parseDouble(formatting.format(a2 - (a2 * b2)));
+            a3 = Double.parseDouble(String.valueOf(a3 - (a2 * b3))); //MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
+            a2 = Double.parseDouble(String.valueOf(a2 - (a2 * b2)));
         }
 
         if (c2 != 0) { // reduce the third row to 0 in the first #
-            c3 = Double.parseDouble(formatting.format(c3 - (b3 * c2))); //MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
-            c2 = Double.parseDouble(formatting.format(c2 - (b2 * c2)));
+            c3 = Double.parseDouble(String.valueOf(c3 - (b3 * c2))); //MAKE THE FIRST NUMBER 0 AND THEN substract ALL THE NUMBERS IN THAT row by whatever we took of in the first one
+            c2 = Double.parseDouble(String.valueOf(c2 - (b2 * c2)));
         }
         if (c3 < 0.005) {
             c3 = 0;
         }
         // second row done
         if (c3 != 1 && c3 != 0) { //if a not equal to one or zero
-            c3 = Double.parseDouble(formatting.format(c3 / c3));//MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
+            c3 = Double.parseDouble(String.valueOf(c3 / c3));//MAKE THE FIRST NUMBER 1 AND THEN DIVIDE ALL THE NUMBERS IN THAT row
         }
 
         if (c3 != 0 && b3 != 0) { // reduce the second row to 0 in the first #
-            b3 = Double.parseDouble(formatting.format(b3 - (b3 * c3)));
+            b3 = Double.parseDouble(String.valueOf(b3 - (b3 * c3)));
         }
 
         if (c3 != 0 && a3 != 0) { // reduce the third row to 0 in the first #
-            a3 = Double.parseDouble(formatting.format(a3 - (a3 * c3)));
+            a3 = Double.parseDouble(String.valueOf(a3 - (a3 * c3)));
         }
         double arr[] = {a1, a2, a3, b1, b2, b3, c1, c2, c3}; // array to hold the matrix
 
@@ -341,9 +340,9 @@ public class Model2for3x3 {
         }
 
         for (int i = 0; i < v1.length; i++) { // loop until we went through all the numbers in the matrix  SHOULD GO THROUGH 3 TIME CUZ 3 ROWS
-            double t11 = Double.parseDouble(formatting.format(v1[i])); // T11 = FIRST NUMBER IN ROW T22= SECOND NUMBER IN ROW T33 THRID NUMBER IN ROW
-            double t22 = Double.parseDouble(formatting.format(v1[i + 1]));
-            double t33 = Double.parseDouble(formatting.format(v1[i + 2]));
+            double t11 = Double.parseDouble(String.valueOf(v1[i])); // T11 = FIRST NUMBER IN ROW T22= SECOND NUMBER IN ROW T33 THRID NUMBER IN ROW
+            double t22 = Double.parseDouble(String.valueOf(v1[i + 1]));
+            double t33 = Double.parseDouble(String.valueOf(v1[i + 2]));
             if (t11 != 0 && t22 != 0 && t33 != 0) { // if the whole row has numbers ( which means other 2 rows are all zero
                 y1.set(0, -t22); // x2
                 y1.set(1, 1.0); //  vector for second number (x2) free number
