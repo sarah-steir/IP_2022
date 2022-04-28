91dd4cfe565eb259637d13542b1e52079c703ad5
natyluh <natalialucena02@gmail.com>
2022-04-28 09:53:58 -0400

    Solved a couple of issues. Implemented new things onto planes (parallel planes added).

--- src/main/java/pack/Controller/Controller3.java
+++ src/main/java/pack/Controller/Controller3.java
@@ -3,36 +3,23 @@ package pack.Controller;
 import javafx.scene.text.Text;
 import pack.Model.Model3;
 import pack.View.Customs.CustomText;
-import pack.View.Customs.CustomTextField;
 import pack.View.View3;
-
-import java.util.ArrayList;
 
 public class Controller3 {
 
     View3 view;
 
-    //Take the graph and do it here bb
     private Model3 model= new Model3();
-
-
     public Controller3(View3 view) {
         this.view = view;
     }
 
-
-    private ArrayList<CustomTextField> fieldList;
-    private ArrayList<Double> matrixCoefficients;
-    private boolean is2by2;
-
-    //Lines
     public void getValues() {
         if(view.getRb1().isSelected()) {
         model.bringT(view.getFieldListRb1());
         model.setThingies(); }
 
-        if(view.getRb2().isSelected()) { {model.transform(view.getFieldListRb2());}}
-    }
+        if(view.getRb2().isSelected()) { {model.transform(view.getFieldListRb2());}}}
 
 
     public String identifyLines(){
@@ -55,8 +42,8 @@ public class Controller3 {
       if(view.getRb2().isSelected()) {
             model.crossProduct(model.n1,model.n2);
             model.solutionPoint();
-            view.getGraph().addPlane(model.n1[0]/-model.n1[3],model.n1[1]/-model.n1[3],model.n1[2]/-model.n1[3],"Plane1");
-            view.getGraph().addPlane(model.n2[0]/-model.n2[3],model.n2[1]/-model.n2[3],model.n2[2]/-model.n2[3],"Plane1");
+            view.getGraph().addPlane(model.n1[3]/-model.n1[0],model.n1[3]/-model.n1[1],model.n1[3]/-model.n1[2],"Plane 1");
+            view.getGraph().addPlane(model.n2[3]/-model.n2[0],model.n2[3]/-model.n2[1],model.n2[3]/-model.n2[2],"Plane 2");
             view.getGraph().addLine(model.solutions[0], model.solutions[1],model.crossProduct(model.n1,model.n2));}
     }
 
@@ -69,37 +56,28 @@ public class Controller3 {
             Text l2 = (Text) view.getGraph().labelsList.get(5);
             CustomText c2 = new CustomText(l2.getText());
             CustomText[] custom = {top,c, c2};
-            return custom;
+            return custom;}
 
-        }
         else {
             CustomText top= new CustomText(" Equations of the planes: ");
             CustomText c= new CustomText(model.planeEq(1));
             CustomText c2= new CustomText(model.planeEq(2));
             CustomText [] custom= {top,c,c2};
-            return custom;
-        }
-
+            return custom;}
     }
 
     public CustomText[] SolutionTexts() {
         if(view.getRb1().isSelected()) {
         if(identifyLines()=="intersecting"){
-            CustomText l= new CustomText("Solution: "+"S="+model.x[0]+"\n"+
-                                   "T="+model.x[1]+"\n");
-
+            CustomText l= new CustomText("Solution: "+"S="+model.x[0]+"\n"+ "T="+model.x[1]+"\n");
             CustomText l2= new CustomText("Intersection point: <"+model.intersectionLines().getX()+", "+model.intersectionLines().getY()
            +", "+model.intersectionLines().getZ()+">");
-
             CustomText [] custom= {l,l2};
-
-            return custom;
-
-        }
+            return custom;}
 
         if(identifyLines()=="skew"){
             CustomText l= new CustomText("These two lines are skew"+"\n"
-                                          +"The closest distance between+" +"\n"+
+                                          +"The closest distance between" +"\n"+
                                           "them is: "+model.distanceSkew());
             CustomText [] custom= {l};
 
@@ -117,6 +95,20 @@ public class Controller3 {
         else {
             CustomText c=new CustomText("Line of intersection:");
             CustomText c1=new CustomText("Direction vector: <"+model.getCrossProduct()[0]+", "+model.getCrossProduct()[1]+", "+model.getCrossProduct()[2]+">");
+
+            String s1=String.valueOf(model.solutions[0].getX());
+            String s2=String.valueOf(model.solutions[0].getY());
+            String s3=String.valueOf(model.solutions[0].getZ());
+
+
+            if(model.getCrossProduct()[0]==0&&model.getCrossProduct()[1]==0&&model.getCrossProduct()[1]==0) {
+                CustomText c4=new CustomText("Paralel planes. No intersection line");
+                CustomText [] custom= {c4};
+
+                return custom;
+            }
+
+
             CustomText c2=new CustomText("Point: <"+model.solutions[0].getX()+", "+model.solutions[0].getY()+", "+model.solutions[0].getZ()+">");
             CustomText [] custom= {c,c1,c2};
 
